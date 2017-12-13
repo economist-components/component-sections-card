75db38f6468e0410fc63ca987eeff5196f973e2b
Jacek Ciolek <jacek@webnicer.com>
2017-12-13 12:10:46 +0000

    fix(link): get rid of filtering out "internal" prop

--- src/parts/section-card-link.js
+++ src/parts/section-card-link.js
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 import Button from '@economist/component-link-button';
 
 export default function SectionCardLink({ buttonClassName, linkClassName, title, buttonProps, prefix }) {
-  const { internal, ...cleanedButtonProps } = buttonProps; // eslint-disable-line no-unused-vars
   const customLinkClassName = linkClassName ?
   `${ prefix }__list-item ${ prefix }__list-item--${ linkClassName }` :
   `${ prefix }__list-item`;
   return (
     <li className={customLinkClassName}>
       <Button
-        {...cleanedButtonProps}
+        {...buttonProps}
         className={buttonClassName}
       >
         {title}
